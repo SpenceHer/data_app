716533d788cd7dcf6f8cecd710f16fd546aae43f
Spencer Smith <57018006+SpenceHer@users.noreply.github.com>
2023-09-10 22:56:06 -0700

    Update main.py

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ visualize_content_frame = tk.Frame(content_frame, bg="beige")
 style.configure("file_button.TButton", background="white", borderwidth=0, padding=0, font=("Arial", 48))
 file_button = ttk.Button(button_frame, text="File", style="file_button.TButton")
 file_button.pack(side="left", fill="x", expand=True)  # Set expand=True to fill the horizontal space
-file_button.config(command=lambda: file_handling.setup_file_tab(style, sub_button_frame, dataframe_content_frame, file_handling_content_frame, editing_content_frame, visualize_content_frame))
+file_button.config(command=lambda: file_handling.setup_file_tab(style, sub_button_frame, dataframe_content_frame, file_handling_content_frame, editing_content_frame, visualize_content_frame, initialize=False))
  
 style.configure("dataframe_view_button.TButton", background="gray", borderwidth=0, padding=0, font=("Arial", 48))
 dataframe_view_button = ttk.Button(button_frame, text="Dataframe View", style="dataframe_view_button.TButton")
@@ -62,7 +62,7 @@ style.configure("visualize_button.TButton", background="gray", borderwidth=0, pa
 visualize_button = ttk.Button(button_frame, text="Visualize Data", style="visualize_button.TButton")
 visualize_button.pack(side="left", fill="x", expand=True)  # Set expand=True to fill the horizontal space
 visualize_button.config(command=lambda: visualize.setup_visualize_tab(style, sub_button_frame, dataframe_content_frame, file_handling_content_frame, editing_content_frame, visualize_content_frame))
- 
+
 
 file_handling.setup_file_tab(style, sub_button_frame, dataframe_content_frame, file_handling_content_frame, editing_content_frame, visualize_content_frame)
  
