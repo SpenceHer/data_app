428b2ffc334a7f076bde0ccc1a5b79be138d9235
Spencer Smith <57018006+SpenceHer@users.noreply.github.com>
2023-09-12 11:37:37 -0700

    Update data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -1,9 +1,35 @@
 
 df = None
+df_dict = {}
+df_name = None
 
-def set_dataframe(dataframe):
+
+def set_dataframe_name(dataframe_name):
+    global df_name
+    df_name = dataframe_name
+
+
+def get_dataframe_name():
+    return df_name
+
+
+
+
+
+def add_dataframe_to_dict(dataframe, dataframe_name):
+    global df_dict
+    df_dict[dataframe_name] = dataframe
+
+def get_df_dict():
+    return df_dict
+
+
+
+
+
+def set_dataframe(dataframe_name):
     global df
-    df = dataframe
+    df = df_dict[dataframe_name]
 
 def get_dataframe():
     return df
