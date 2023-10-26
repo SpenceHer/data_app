ce592462b0cb6da71a4e7e6078e44adf8f439d77
Spencer Smith <57018006+SpenceHer@users.noreply.github.com>
2023-10-26 11:03:36 -0700

    Update data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -2,6 +2,7 @@
 df = None
 df_dict = {}
 df_name = None
+tab_dict = {}
 
 
 def set_dataframe_name(dataframe_name):
@@ -33,3 +34,15 @@ def set_dataframe(dataframe_name):
 
 def get_dataframe():
     return df
+
+
+
+
+
+
+def add_tab_to_dict(tab_name, setting):
+    global tab_dict
+    tab_dict[tab_name] = setting
+
+def get_tab_dict():
+    return tab_dict
