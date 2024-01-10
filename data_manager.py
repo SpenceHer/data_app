17eabc0e3827d95b552505a1ca5cd4fbb34573b9
Spencer Smith <57018006+SpenceHer@users.noreply.github.com>
2024-01-10 09:13:52 -0800

    Update data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -113,16 +113,9 @@ def get_comp_tab_data_selection():
 
 reg_tab_dep_var = None
 reg_tab_ind_var_list = []
-reg_tab_ind_var_dict = {}
 
 reg_tab_selected_regression = ""
 
-lin_reg_ind_dict = {}
-
-
-reg_tab_log_reg_target_value_dict = {}
-reg_tab_log_reg_ind_dict = {}
-reg_tab_log_reg_ref_dict = {}
 
 # REGRESSION DEPENDENT VARIABLE
 def set_reg_tab_dep_var(variable):
@@ -152,20 +145,13 @@ def get_reg_tab_selected_regression():
     return reg_tab_selected_regression
 
 
-# LINEAR REGRESSION INDEPENDENT VARIABLES DICT
-def add_variable_to_lin_reg_ind_dict(tab_name, setting):
-    global lin_reg_ind_dict
-    lin_reg_ind_dict[tab_name] = setting
 
-def get_lin_reg_ind_dict():
-    return lin_reg_ind_dict
 
 
 
 reg_tab_log_reg_target_value_dict = {}
 log_reg_var_type_dict = {}
 log_reg_ref_dict = {}
-
 
 
 # LOGISTIC REGRESSION TARGET VALUE
@@ -178,18 +164,18 @@ def get_reg_tab_log_reg_target_value_dict():
 
 
 # LOGISTIC REGRESSION INDEPENDENT VARIABLES DICT
-def add_variable_to_log_reg_var_type_dict(tab_name, setting):
+def add_variable_to_log_reg_var_type_dict(variable, input_var):
     global log_reg_var_type_dict
-    log_reg_var_type_dict[tab_name] = setting
+    log_reg_var_type_dict[variable] = input_var
 
 def get_log_reg_var_type_dict():
     return log_reg_var_type_dict
 
 
 # LOGISTIC REGRESSION REFERENCES VARIABLES DICT
-def add_variable_to_log_reg_ref_dict(tab_name, setting):
+def add_variable_to_log_reg_ref_dict(ind_var, reference_value):
     global log_reg_ref_dict
-    log_reg_ref_dict[tab_name] = setting
+    log_reg_ref_dict[ind_var] = reference_value
 
 def get_log_reg_ref_dict():
     return log_reg_ref_dict
@@ -204,6 +190,60 @@ def get_log_reg_ref_dict():
 #####################################################################
 #####################################################################
 
+# NON-NUMERIC INDEPENDENT VARIABLES DICT
+non_numeric_ind_dict = {}
+
+
+def add_variable_to_non_numeric_ind_dict(ind_var, value, input_var):
+    global non_numeric_ind_dict
+    
+    # Check if ind_var exists in the dictionary, if not, create an empty dictionary
+    if ind_var not in non_numeric_ind_dict:
+        non_numeric_ind_dict[ind_var] = {}
+    
+    non_numeric_ind_dict[ind_var][value] = input_var
+
+def get_non_numeric_ind_dict():
+    return non_numeric_ind_dict
+
+
+
+
+#####################################################################
+#####################################################################
+#####################################################################
+
+mach_learn_tab_dep_var = None
+mach_learn_tab_ind_var_list = []
+
+mach_learn_tab_selected_model = None
+
+# REGRESSION DEPENDENT VARIABLE
+def set_mach_learn_tab_dep_var(variable):
+    global mach_learn_tab_dep_var
+    mach_learn_tab_dep_var = variable
+
+def get_mach_learn_tab_dep_var():
+    return mach_learn_tab_dep_var
+
+
+# REGRESSION INDEPENDENT VARIABLES LIST
+def add_variable_to_mach_learn_tab_ind_var_list(variable):
+    global mach_learn_tab_ind_var_list
+    if variable not in mach_learn_tab_ind_var_list:
+        mach_learn_tab_ind_var_list.append(variable)
+
+def get_mach_learn_tab_ind_var_list():
+    return mach_learn_tab_ind_var_list
+
+
+# REGRESSION ANALYSIS TYPE
+def set_mach_learn_tab_selected_model(variable):
+    global mach_learn_tab_selected_model
+    mach_learn_tab_selected_model = variable
+
+def get_mach_learn_tab_selected_model():
+    return mach_learn_tab_selected_model
 
 
 
@@ -217,11 +257,13 @@ def get_log_reg_ref_dict():
 
 
 
+create_var_tab_var_list = []
 
+# REGRESSION INDEPENDENT VARIABLES LIST
+def add_variable_to_create_var_tab_var_list(variable):
+    global create_var_tab_var_list
+    if variable not in create_var_tab_var_list:
+        create_var_tab_var_list.append(variable)
 
-
-
-
-#####################################################################
-#####################################################################
-#####################################################################
+def get_create_var_tab_var_list():
+    return create_var_tab_var_list
