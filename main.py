734e0e2c2a72537804104f55c567eb60678aa491
Spencer Smith <57018006+SpenceHer@users.noreply.github.com>
2024-01-17 21:11:33 -0800

    Update main.py

--- main.py
+++ main.py
@@ -50,22 +50,22 @@ visualize_content_frame = tk.Frame(content_frame, bg="beige")
 # Add Buttons
 
 # Banner Frame
-style.configure("file_button.TButton", background="white", borderwidth=0, padding=0, font=("Arial", 48))
+style.configure("file_button.TButton", background="white", borderwidth=0, padding=0, font=("Arial", 48, 'bold'))
 file_button = ttk.Button(button_frame, text="File", style="file_button.TButton")
 file_button.pack(side="left", fill="x", expand=True)  # Set expand=True to fill the horizontal space
 file_button.config(command=lambda: file_handling.setup_file_tab(style, sub_button_frame, dataframe_content_frame, file_handling_content_frame, editing_content_frame, visualize_content_frame, initialize=False))
  
-style.configure("dataframe_view_button.TButton", background="gray", borderwidth=0, padding=0, font=("Arial", 48))
+style.configure("dataframe_view_button.TButton", background="gray", borderwidth=0, padding=0, font=("Arial", 48, 'bold'))
 dataframe_view_button = ttk.Button(button_frame, text="Dataframe View", style="dataframe_view_button.TButton")
 dataframe_view_button.pack(side="left", fill="x", expand=True)  # Set expand=True to fill the horizontal space
 dataframe_view_button.config(command=lambda: file_handling.setup_dataframe_view_tab(style, sub_button_frame, dataframe_content_frame, file_handling_content_frame, editing_content_frame, visualize_content_frame, initialize=False))
  
-style.configure("edit_button.TButton", background="gray", borderwidth=0, padding=0, font=("Arial", 48))
+style.configure("edit_button.TButton", background="gray", borderwidth=0, padding=0, font=("Arial", 48, 'bold'))
 edit_button = ttk.Button(button_frame, text="Edit/Clean Data", style="edit_button.TButton")
 edit_button.pack(side="left", fill="x", expand=True)  # Set expand=True to fill the horizontal space
 edit_button.config(command=lambda: edit.setup_edit_tab(style, sub_button_frame, dataframe_content_frame, file_handling_content_frame, editing_content_frame, visualize_content_frame))
  
-style.configure("visualize_button.TButton", background="gray", borderwidth=0, padding=0, font=("Arial", 48))
+style.configure("visualize_button.TButton", background="gray", borderwidth=0, padding=0, font=("Arial", 48, 'bold'))
 visualize_button = ttk.Button(button_frame, text="Visualize Data", style="visualize_button.TButton")
 visualize_button.pack(side="left", fill="x", expand=True)  # Set expand=True to fill the horizontal space
 visualize_button.config(command=lambda: visualize.setup_visualize_tab(style, sub_button_frame, dataframe_content_frame, file_handling_content_frame, editing_content_frame, visualize_content_frame))
